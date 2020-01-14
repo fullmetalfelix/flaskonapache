43333f4d4e6be22ad8462b91c59904482a4b8cfa
fullmetalfelix <vampirer@tiscalinet.it>
2020-01-14 10:46:49 +0200

    4

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 echo starting uwsgi server
 #uwsgi -s /tmp/app.sock --manage-script-name --mount /=app:app --plugin python3
-uwsgi --socket 0.0.0.0:8080 --protocol=http -w wsgi:app --plugin python3
+uwsgi --socket 0.0.0.0:5000 --protocol=http --wsgi-file wsgi.py --plugin python3
