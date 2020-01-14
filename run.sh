7ac93b6c63b2d215d3abd60873a85b58b5fd346d
Federici Canova Filippo <federif1@t30400-la051.org.aalto.fi>
2020-01-14 09:56:31 +0100

    5

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 echo starting uwsgi server
 #uwsgi -s /tmp/app.sock --manage-script-name --mount /=app:app --plugin python3
-uwsgi --socket 0.0.0.0:5000 --protocol=http --wsgi-file wsgi.py --plugin python3
+#uwsgi --socket 0.0.0.0:5000 --protocol=http --wsgi-file wsgi.py --plugin python3
+uwsgi --ini labcore.ini
