7585e873ee0b9c1cabc150c49015455bb86ba00f
Federici Canova Filippo <federif1@t30400-la051.org.aalto.fi>
2020-01-14 09:30:39 +0100

    3

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #uwsgi -s /tmp/app.sock --manage-script-name --mount /=app:app --plugin python3
-uwsgi --socket 0.0.0.0:5000 --protocol=http -w wsgi:app --plugin python3
+uwsgi --socket 0.0.0.0:8080 --protocol=http -w wsgi:app --plugin python3
