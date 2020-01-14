b62a89e954a8bb89a9dc734b54f496184af90b01
Federici Canova Filippo <federif1@t30400-la051.org.aalto.fi>
2020-01-14 09:24:35 +0100

    3

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,12 @@
-from test import app as application
+from app import app as application
 
 if __name__ == '__main__':
+
+	app.run()
+	'''
     from wsgiref.simple_server import make_server
     httpd = make_server('localhost', 8051, application)
     print("Serving at http://localhost:8051/ \n PRESS CTRL+C to Terminate. \n")
     httpd.serve_forever()
     print("Terminated!!")
-    
+    '''
